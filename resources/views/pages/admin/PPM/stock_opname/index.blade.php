ea8e749aab3872c511b0da98bee1340d3de9096c
fahillan18899 <fahillan@gmail.com>
2023-08-29 15:31:31 +0700

    integrasi tampilan halaman dan fungsinya

---
+++ resources/views/pages/admin/PPM/stock_opname/index.blade.php
@@ -0,0 +1,141 @@
+@extends('layouts.admin')
+
+@section('content')
+<?php
+$alert = "";
+
+if (isset($_POST['tambah_opname'])) {
+  $stock = $_POST['jumlah_masuk'] - $_POST['jumlah_keluar'];
+  $sql_tambah = "INSERT INTO stock_opname (`id`, `nama`, `jenis`, `lokasi_pemakaian`, `jumlah_masuk`, `jumlah_keluar`, `stock`, `kode_rs`) VALUES ('{$_POST['id']}', '{$_POST['nama']}', '{$_POST['jenis']}','{$_POST['lokasi_pemakaian']}','{$_POST['jumlah_masuk']}','{$_POST['jumlah_keluar']}','$stock','{$_POST['kodeRs']}')";
+  mysqli_query($db, $sql_tambah);
+
+  $alert = "tambah";
+}
+
+if (isset($_POST['update_opname'])) {
+  $stock = $_POST['jumlah_masuk'] - $_POST['jumlah_keluar'];
+  $sql_ubah = "UPDATE stock_opname SET 
+    nama='{$_POST['nama']}', 
+    jenis='{$_POST['jenis']}', 
+    lokasi_pemakaian='{$_POST['lokasi_pemakaian']}', 
+    jumlah_masuk='{$_POST['jumlah_masuk']}', 
+    stock='$stock', 
+    jumlah_keluar='{$_POST['jumlah_keluar']}'
+    WHERE id='{$_POST['id']}'";
+  mysqli_query($db, $sql_ubah);
+
+  $alert = "ubah";
+}
+
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+
+  <section class="content-header">
+
+    <div class="p-l-30 p-r-30">
+      <div class="header-icon"><i class="pe-7s-world"></i></div>
+      <div class="header-title">
+        <h1>Stock Opname</h1>
+        <small>Tabel Stock Opname</small>
+      </div>
+    </div>
+  </section>
+  <!-- Main content -->
+  <div class="content">
+    <!-- demo mode enable alert -->
+    <div id="demoModeEnable"></div>
+    <!-- alert message -->
+
+    <!-- content -->
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="panel panel-default thumbnail">
+
+          <div class="panel-heading no-print">
+            <div class="btn-group">
+              <a class="btn btn-success" href="?hal=stock_opname&fun=create"><i class="fa fa-plus"></i> Tambah Stock Opname </a>
+            </div>
+          </div>
+
+          <div class="panel-body panel-form">
+            <?php if ($alert == "tambah") { ?>
+              <div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <h5> Berhasil</h5>
+                Data sudah ditambahkan.
+              </div>
+            <?php } else if ($alert == "hapus") { ?>
+              <div class="alert alert-danger alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <h5> Berhasil</h5>
+                Data sudah dihapus.
+              </div>
+            <?php } else if ($alert == "ubah") { ?>
+              <div class="alert alert-warning alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <h5> Berhasil</h5>
+                Data sudah diubah.
+              </div>
+            <?php } ?>
+            <div class="row">
+              <div class="col-md-12 col-sm-12">
+
+                <!--TABEL-->
+                <table class="datatable table table-striped table-bordered" style="width:100%">
+                  <thead class="table-light">
+                    <tr>
+                      <th scope="col">No</th>
+                      <th scope="col">Nama SparePart</th>
+                      <th scope="col">Type</th>
+                      <th scope="col">Lokasi Pemakaian</th>
+                      <th scope="col">Jumlah Masuk</th>
+                      <th scope="col">Jumlah Keluar</th>
+                      <th scope="col">Total</th>
+                      <th scope="col">Tombol_Aksi_Table</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @forelse ($items as $item)
+                    <tr>
+                      <td>{{ $item->id }}</td>
+                      <td>{{ $item->nama }}</td>
+                      <td>{{ $item->jenis }}</td>
+                      <td>{{ $item->lokasi_pemakaian }}</td>
+                      <td>{{ $item->jumlah_masuk}}</td>
+                      <td>{{ $item->jumlah_keluar}}</td>
+                      <td>{{ $item->stock}}</td>
+                      <td>{{ $item->kode_rs }}</td>  
+                      <td>
+                        <a href="{{ url('dashboard', $item->id) }}" class="btn btn-info"> <i class="fa fa-pencil-alt"></i> </a>
+                        <form action="{{ url('dashboard', $item->id) }}" method="POST" class="d-inline">
+                          @csrf
+                          @method('delete')
+                          <button class="btn btn-danger">
+                            <i class="fa fa-trash"></i>
+                          </button>
+                        </form>
+                      </td>
+                    </tr>
+                    @empty
+                    <tr>
+                      <td class="text-center" colspan="7">Data Kosong</td>
+                    </tr>
+                    @endforelse
+                  </tbody>
+                </table>
+                <!--TABEL-->
+              </div>
+              <div class="col-md-3"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div> <!-- /.content -->
+</div> <!-- /.content-wrapper -->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\DataKelengkapanController;
+use App\Http\Controllers\Admin\PPM\DataKelengkapanController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Admin\PPM\RegistrasiAset;
+use App\Http\Controllers\Admin\PPM\StockOpnameController;
+use App\Http\Controllers\Admin\PPM\Operator;
+use App\Http\Controllers\Admin\PPM\LaporanKegiatanController;
+use App\Http\Controllers\Admin\PPM\AnalisisDataController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,18 +32,18 @@ Route::prefix('dashboard')->group(function () {
 
     Route::prefix('ppm')->group(function () {
         Route::get('/home', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/data_kelengkapan', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('/data_kelengkapan', DataKelengkapanController::class);
         Route::resource('/registrasi', RegistrasiAset::class);
-        Route::get('/data_inventaris', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/aset_teregistrasi', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/aset_unregistrasi', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/aset_non_alkes', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/lembar_pemeliharaan', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/jadwal_pemeliharaan', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/laporan_kegiatan', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/operator', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/stock_opname', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/analisis_data', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('/data_inventaris', DashboardController::class);
+        Route::resource('/aset_teregistrasi', DashboardController::class);
+        Route::resource('/aset_unregistrasi', DashboardController::class);
+        Route::resource('/aset_non_alkes', DashboardController::class);
+        Route::resource('/lembar_pemeliharaan', DashboardController::class);
+        Route::resource('/jadwal_pemeliharaan', DashboardController::class);
+        Route::resource('/laporan_kegiatan', LaporanKegiatanController::class);
+        Route::resource('/operator', Operator::class);
+        Route::resource('/stock_opname', StockOpnameController::class);
+        Route::resource('/analisis_data', AnalisisDataController::class);
     });
 });
 
