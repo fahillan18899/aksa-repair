6645296a319bd37ade2a499bc283515fbe9be502
ilzamafif <ilzamafif27@gmail.com>
2023-10-17 08:39:44 +0700

    menghubungkan data terkalibrasi dengan tanggal kalibrasi data inventaris

--- app/Http/Controllers/Admin/PPM/RegistrasiAsetController.php
+++ app/Http/Controllers/Admin/PPM/RegistrasiAsetController.php
@@ -6,6 +6,7 @@ use App\Exports\RegistrasiAssetsExport;
 use App\Http\Controllers\Controller;
 use App\Imports\RegistrasiAsetsImport;
 use App\Models\Alat;
+use App\Models\LembarPemeliharaan;
 use Illuminate\Http\Request;
 use App\Models\Registrasi;
 use App\Models\Ruangan;
@@ -100,10 +101,13 @@ class RegistrasiAsetController extends Controller
             'gambar.max' => 'Ukuran Gambar Maksimal 4MB'
         ]);
 
-        $data['gambar'] = $request->file('gambar')->store(
-            'assets/gallery',
-            'public'
-        );
+        if (isset($data['gambar'])) {
+            $data['gambar'] = $request->file('gambar')->store(
+                'assets/gallery',
+                'public'
+            );
+        }
+
 
         $data['umur_alat'] = date("Y") - $data['tahun_perolehan'];
         function hitung($tahunPenyusutan, $harga_perolehan)
@@ -115,6 +119,68 @@ class RegistrasiAsetController extends Controller
         }
         $data['penyusutan_aset'] = hitung($data['umur_alat'], $data['tahun_perolehan']);
         $data['kode_rs'] =  Auth::user()->kode_rs;
+
+        $res = [
+            'tanggal' => $data['tanggal_kalibrasi'],
+            'kegiatan' => 'Pemelihraan',
+            'engineer' => '',
+            'id_aset' => $data['id_aset'],
+            'nama_alat' => $data['nama_alat'],
+            'serial_number' => $data['serial_number'],
+            'merek' => $data['merek'],
+            'instalasi' => '',
+            'tipe' => $data['type'],
+            'ruangan' => $data['lokasi_alat'],
+            'hand_hygiene' => '',
+            'menyiapkan_alat_dan_bahan' => '',
+            'alat_pelindung_diri' => '',
+            'mengoprasikan_alat_kalibrasi' => '',
+            'ktd' => '',
+            'mengoprasikan_alat' => '',
+            'identifikasi_bahaya' => '',
+            'badan_selungkup1' => '',
+            'badan_selungkup2' => '',
+            'alat_sistem_interlock1' => '',
+            'alat_sistem_interlock2' => '',
+            'kabel_kelenturan1' => '',
+            'kabel_kelenturan2' => '',
+            'sistem_pengunci1' => '',
+            'sistem_pengunci2' => '',
+            'tombol_saklar1' => '',
+            'tombol_saklar2' => '',
+            'label_penandaan1' => '',
+            'label_penandaan2' => '',
+            'display_layar1' => '',
+            'display_layar2' => '',
+            'aksesoris1' => '',
+            'aksesoris2' => '',
+            'indikator_bunyi1' => '',
+            'indikator_bunyi2' => '',
+            'pembersihan' => '',
+            'pengencangan_bagian_alat' => '',
+            'pelumasan' => '',
+            'kalibrasi_berkala' => '',
+            'penggantian_bahan_habis_pakai' => '',
+            'cek_alat' => '',
+            'nama_sukucadang' => '',
+            'volume' => '',
+            'harga_satuan' => '',
+            'jumlah_harga' => '',
+            'evaluasi' => '',
+            'status' => '',
+            'status1' => '',
+            'mulai_bekerja' => '',
+            'selesai_kerja' => '',
+            'durasi' => '',
+            'user' => '',
+            'engginer' => '',
+            'kode_rs' => ''
+        ];
+
+        $res['kode_rs'] = Auth::user()->kode_rs;
+        if (isset($data['tanggal_kalibrasi'])) {
+            LembarPemeliharaan::create($res);
+        }
 
         Registrasi::create($data);
 
