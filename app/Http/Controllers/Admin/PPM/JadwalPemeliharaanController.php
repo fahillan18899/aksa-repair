15e1b614ee4ade00716453036c4ffaca2d3f6146
ilzamafif <ilzamafif27@gmail.com>
2024-01-23 14:43:08 +0700

    update jadwal pemeliharaan

--- app/Http/Controllers/Admin/PPM/JadwalPemeliharaanController.php
+++ app/Http/Controllers/Admin/PPM/JadwalPemeliharaanController.php
@@ -20,7 +20,7 @@ class JadwalPemeliharaanController extends Controller
     public function state()
     {
         $items = JadwalPemeliharaan::where('kode_rs', Auth::user()->kode_rs)->get();
-        $states = DB::table("registrasis")->distinct('lokasi_alat')->pluck('lokasi_alat', 'id_aset');
+        $states = DB::table("registrasis")->where('kode_rs', Auth::user()->kode_rs)->distinct('lokasi_alat')->pluck('lokasi_alat', 'id_aset');
 
         return view('pages.admin.PPM.jadwal_pemeliharaan.index', [
 
@@ -32,14 +32,14 @@ class JadwalPemeliharaanController extends Controller
     public function city($id)
     {
         $cities = DB::table("registrasis")
-        ->where("lokasi_alat", $id)->where('kode_rs', Auth::user()->kode_rs)->get()
+        ->where("lokasi_alat", $id)->where('kode_rs', Auth::user()->kode_rs)
             ->pluck('nama_alat', 'id_aset');
         return json_encode($cities);
     }
 
     public function updateStatus($id)
     {
-        $item = JadwalPemeliharaan::where('id', $id)->where('kode_rs', Auth::user()->kode_rs)->get()->first();
+        $item = JadwalPemeliharaan::where('id', $id)->where('kode_rs', Auth::user()->kode_rs)->first();
         if ($item) {
             if ($item->status == '0') {
                 $item->status = '1';
