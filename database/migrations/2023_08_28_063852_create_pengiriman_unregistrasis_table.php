2ebbc03a6183497621b62751342bd237743fa6fd
ilzamafif <ilzamafif27@gmail.com>
2024-01-24 11:44:53 +0700

    perbaikan struktur database aset unregistrasi

--- database/migrations/2023_08_28_063852_create_pengiriman_unregistrasis_table.php
+++ database/migrations/2023_08_28_063852_create_pengiriman_unregistrasis_table.php
@@ -15,23 +15,23 @@ return new class extends Migration
     {
         Schema::create('pengiriman_unregistrasis', function (Blueprint $table) {
             $table->string('id_perbaikan_un');
-            $table->date('tanggal_perbaikan_un');        
-            $table->date('tanggal_pengiriman_un');        
-            $table->string('nama_alat_un');        
-            $table->string('merek_alat_un');        
-            $table->string('type_alat_un');        
-            $table->string('serial_number_un');        
-            $table->string('lokasi_alat_un');        
-            $table->string('pelapor_un');        
-            $table->string('keterangan_un');        
-            $table->string('teknisi_1_un');        
-            $table->string('teknisi_2_un');        
-            $table->string('teknisi_3_un');        
-            $table->string('nama_rekanan_un');        
-            $table->string('alamat_rekanan_un');        
-            $table->string('teknisi_rekanan_un');        
-            $table->string('telphone_teknisi_rek_un');        
-            $table->string('ka_instalasi_un');
+            $table->date('tanggal_perbaikan_un')->nullable();        
+            $table->date('tanggal_pengiriman_un')->nullable();        
+            $table->string('nama_alat_un')->nullable();        
+            $table->string('merek_alat_un')->nullable();        
+            $table->string('type_alat_un')->nullable();        
+            $table->string('serial_number_un')->nullable();        
+            $table->string('lokasi_alat_un')->nullable();        
+            $table->string('pelapor_un')->nullable();        
+            $table->string('keterangan_un')->nullable();        
+            $table->string('teknisi_1_un')->nullable();        
+            $table->string('teknisi_2_un')->nullable();        
+            $table->string('teknisi_3_un')->nullable();        
+            $table->string('nama_rekanan_un')->nullable();        
+            $table->string('alamat_rekanan_un')->nullable();        
+            $table->string('teknisi_rekanan_un')->nullable();        
+            $table->string('telphone_teknisi_rek_un')->nullable();        
+            $table->string('ka_instalasi_un')->nullable();
             $table->string('kode_rs', 10);
             $table->integer('active')->default(1);
 
--- database/migrations/2023_08_28_064830_create_pengembalian_unregistrasis_table.php
+++ database/migrations/2023_08_28_064830_create_pengembalian_unregistrasis_table.php
@@ -15,27 +15,27 @@ return new class extends Migration
     {
         Schema::create('pengembalian_unregistrasis', function (Blueprint $table) {
             $table->string('id_perbaikan_un');
-            $table->date('tanggal_perbaikan_un');
-            $table->date('tanggal_pengembalian_un');
-            $table->string('nama_alat_un');
-            $table->string('peneriama_alat_un');
-            $table->string('merek_alat_un');
-            $table->string('ka_instalasi_un');
-            $table->string('type_alat_un');
-            $table->string('teknisi_1_un');
-            $table->string('serial_number_un');
-            $table->string('teknisi_2_un');
-            $table->string('teknisi_3_un');
-            $table->string('lokasi_alat_un');
-            $table->string('keterangan_un');
-            $table->string('pelapor_un');
-            $table->string('harga_perbaikan_un');
-            $table->string('penyebab_kerusakan_un');
-            $table->string('pengujian_suku_cadang_un');
-            $table->string('uji_fungsi_setelah_perbaikan_un');
-            $table->string('solusi_perbaikan_un');
-            $table->string('penggantian_suku_cadang_un');
-            $table->string('hasil_verifikasi_un');
+            $table->date('tanggal_perbaikan_un')->nullable();
+            $table->date('tanggal_pengembalian_un')->nullable();
+            $table->string('nama_alat_un')->nullable();
+            $table->string('peneriama_alat_un')->nullable();
+            $table->string('merek_alat_un')->nullable();
+            $table->string('ka_instalasi_un')->nullable();
+            $table->string('type_alat_un')->nullable();
+            $table->string('teknisi_1_un')->nullable();
+            $table->string('serial_number_un')->nullable();
+            $table->string('teknisi_2_un')->nullable();
+            $table->string('teknisi_3_un')->nullable();
+            $table->string('lokasi_alat_un')->nullable();
+            $table->string('keterangan_un')->nullable();
+            $table->string('pelapor_un')->nullable();
+            $table->string('harga_perbaikan_un')->nullable();
+            $table->string('penyebab_kerusakan_un')->nullable();
+            $table->string('pengujian_suku_cadang_un')->nullable();
+            $table->string('uji_fungsi_setelah_perbaikan_un')->nullable();
+            $table->string('solusi_perbaikan_un')->nullable();
+            $table->string('penggantian_suku_cadang_un')->nullable();
+            $table->string('hasil_verifikasi_un')->nullable();
             $table->string('kode_rs', 10);
             $table->integer('active')->default(1);
 
