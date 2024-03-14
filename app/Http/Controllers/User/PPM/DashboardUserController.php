41eeca12214258ca1ce3b3153be36d5a33348744
ilzamafif <ilzamafif27@gmail.com>
2024-03-14 09:10:39 +0700

    membuat api /aset user dan factory table aset registrasi

--- app/Http/Controllers/User/PPM/DashboardUserController.php
+++ app/Http/Controllers/User/PPM/DashboardUserController.php
@@ -9,9 +9,17 @@ use App\Models\PerbaikanUnregistrasi;
 use App\Models\Registrasi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use DataTables;
 
 class DashboardUserController extends Controller
 {
+    public function json()
+    {
+        $query = Registrasi::query()->select(['id_aset', 'jenis_alat', 'nama_alat', 'merek', 'type', 'gambar', 'serial_number', 'lokasi_alat', 'tanggal_kalibrasi', 'distributor', 'distributor', 'alamat_distributor', 'tlp_distributor', 'email_distributor', 'teknisi_distributor', 'tlp_t_distributor', 'no_sertifikat_kalibrasi', 'teknisi_ppm', 'harga_perolehan', 'sumber_dana', 'tahun_perolehan', 'akl', 'akd', 'no_inventaris_1', 'umur_alat', 'jadwal_pemeliharaan'])->where('kode_rs', Auth::user()->kode_rs);
+        $c = Datatables::eloquent($query)->make(false);
+        return $c;
+    }
+    
     public function index()
     {
         $registrasi = Registrasi::where('kode_rs',Auth::user()->kode_rs)->count();
