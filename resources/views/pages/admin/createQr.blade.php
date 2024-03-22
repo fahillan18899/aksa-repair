991aa8775f5a062f318fd7a88707cc6cb81a12a6
fahillan18899 <fahillan@gmail.com>
2024-03-22 13:33:39 +0700

    Penghapusan db tidak terpakai

--- resources/views/pages/admin/createQr.blade.php
+++ resources/views/pages/admin/createQr.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-@section('title', 'Human Resource')
+@section('title', 'Generate QR')
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
  <!-- Content Header (Page header) -->
