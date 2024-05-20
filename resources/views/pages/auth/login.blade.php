6ff3d2eeeed72b07bb4f51ab7316b8606c206438
fahillan18899 <fahillan@gmail.com>
2024-05-20 09:16:45 +0700

    Penambahan RS PKU Muhammadiyah

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -72,6 +72,7 @@
                 <option value="RS0005">RSI Pondok Kopi</option>
                 <option value="RS0006">RSUD Temanggung</option>
                 <option value="RS0007">RSU Ja'far Medika</option>
+                <option value="RS0008">RS PKU Muhammadiyah Wonosobo</option>
               </select>
             </div>
             <div class="form-group">
