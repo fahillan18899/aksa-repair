5c5d3a9d4ccee609ffbf356b4f17abcfe77f6a31
fahillan18899 <fahillan@gmail.com>
2024-08-28 07:58:22 +0700

    penambahan customer : RSIU BOYOLALI

--- database/migrations/2023_08_29_062132_create_stock_opnames_table.php
+++ database/migrations/2023_08_29_062132_create_stock_opnames_table.php
@@ -19,6 +19,11 @@ return new class extends Migration
             $table->date('tanggal_masuk')->nullable();
             $table->date('tanggal_keluar')->nullable();
             $table->integer('stock');
+            $table->string('harga_part');
+            $table->string('jumlah_harga_part');
+            $table->string('id_aset_part');
+            $table->string('nama_alat_pengguna_part');
+            $table->string('lokasi_alat_pengguna_part');
             $table->string('kode_rs');
 
             $table->timestamps();
