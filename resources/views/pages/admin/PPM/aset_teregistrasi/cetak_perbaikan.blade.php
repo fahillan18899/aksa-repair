25a4052dedae38e0bf63ede5ee4b449b195af616
fahillan18899 <fahillan@gmail.com>
2024-11-08 15:17:24 +0700

    perbaikan gambar kopsurat

--- resources/views/pages/admin/PPM/aset_teregistrasi/cetak_perbaikan.blade.php
+++ resources/views/pages/admin/PPM/aset_teregistrasi/cetak_perbaikan.blade.php
@@ -66,7 +66,7 @@
               <img src="{{ url('assets/kop-surat/xxx.png') }}" alt="Kop PKU Muhammadiyah Wonosobo" width="100%">
               @endif
               @if(Auth::user()->user_role == 'admin' && Auth::user()->kode_rs == "RS0017")
-              <img src="{{ url('assets/images/kop_surat_kendal.png') }}" alt="Kop Darul Istiqomah Kendal" width="100%">
+              <img src="{{ url('/assets/images/kop_surat_kendal.png') }}" alt="Kop Darul Istiqomah Kendal" width="100%">
               @endif
             </div>
             <div class="card-body">
