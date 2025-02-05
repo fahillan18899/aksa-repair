7676543f64898141ceef3f3a1d807f3786552fff
fahillan18899 <fahillan@gmail.com>
2025-02-05 14:05:19 +0700

    perbaikan ls inspeksi ke-3

--- resources/views/pages/admin/PPM/lk_inspeksi/data.blade.php
+++ resources/views/pages/admin/PPM/lk_inspeksi/data.blade.php
@@ -214,5 +214,326 @@
 </div> <!-- /.content-wrapper -->
 @endsection
 @push('addon-script')
-<script src="{{ asset('js/lkInspeksi_dataView.js') }}"></script>
+<script>
+  // FUNGSI PRINT
+function printTableMonitoring() {
+  var printContent = document.getElementById("printContent").outerHTML;
+  var originalContent = document.body.innerHTML;
+  document.body.innerHTML = 
+  `<html>
+    <head>
+      <title>Print Table</title>
+      <style>
+        table {
+          width: 100%;
+          border-collapse: collapse;
+        }
+        th, td {
+          border: 1px solid black;
+          padding: 8px;
+          text-align: center;
+        }
+        th {
+          background-color:rgb(241, 236, 236);
+          }
+      </style>
+    </head>
+    <body>
+        ${printContent}
+    </body>
+  </html`;
+  window.print();
+  document.body.innerHTML = originalContent;
+}
+// FUNGSI PRINT END
+</script>
+<script>
+  // FUNGSI TTD DIGITAL
+    // ttd 1
+    (function() {
+      window.requestAnimFrame = (function(callback) {
+        return window.requestAnimationFrame ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame ||
+          window.oRequestAnimationFrame ||
+          window.msRequestAnimaitonFrame ||
+          function(callback) {
+            window.setTimeout(callback, 1000 / 60);
+          };
+      })();
+
+      var canvas = document.getElementById("ttd_canvas1");
+      var ctx = canvas.getContext("2d");
+      ctx.strokeStyle = "#222222";
+      ctx.lineWidth = 4;
+
+      var drawing = false;
+      var mousePos = {
+        x: 0,
+        y: 0
+      };
+
+
+      // --
+      var lastPos = mousePos;
+
+      canvas.addEventListener("mousedown", function(e) {
+        drawing = true;
+        lastPos = getMousePos(canvas, e);
+      }, false);
+
+      canvas.addEventListener("mouseup", function(e) {
+        drawing = false;
+      }, false);
+
+      canvas.addEventListener("mousemove", function(e) {
+        mousePos = getMousePos(canvas, e);
+      }, false);
+      // --
+
+      // Add touch event support for mobile
+      canvas.addEventListener("touchstart", function(e) {
+
+      }, false);
+
+      canvas.addEventListener("touchmove", function(e) {
+        var touch = e.touches[0];
+        var me = new MouseEvent("mousemove", {
+          clientX: touch.clientX,
+          clientY: touch.clientY
+        });
+        canvas.dispatchEvent(me);
+      }, false);
+
+      canvas.addEventListener("touchstart", function(e) {
+        mousePos = getTouchPos(canvas, e);
+        var touch = e.touches[0];
+        var me = new MouseEvent("mousedown", {
+          clientX: touch.clientX,
+          clientY: touch.clientY
+        });
+        canvas.dispatchEvent(me);
+      }, false);
+
+      canvas.addEventListener("touchend", function(e) {
+        var me = new MouseEvent("mouseup", {});
+        canvas.dispatchEvent(me);
+      }, false);
+
+      function getMousePos(canvasDom, mouseEvent) {
+        var rect = canvasDom.getBoundingClientRect();
+        return {
+          x: mouseEvent.clientX - rect.left,
+          y: mouseEvent.clientY - rect.top
+        }
+      }
+
+      function getTouchPos(canvasDom, touchEvent) {
+        var rect = canvasDom.getBoundingClientRect();
+        return {
+          x: touchEvent.touches[0].clientX - rect.left,
+          y: touchEvent.touches[0].clientY - rect.top
+        }
+      }
+
+      function renderCanvas() {
+        if (drawing) {
+          ctx.moveTo(lastPos.x, lastPos.y);
+          ctx.lineTo(mousePos.x, mousePos.y);
+          ctx.stroke();
+          lastPos = mousePos;
+        }
+      }
+      // Add touch event support for mobile N
+
+      // Prevent scrolling when touching the canvas
+      document.body.addEventListener("touchstart", function(e) {
+        if (e.target == canvas) {
+          e.preventDefault();
+        }
+      }, false);
+      document.body.addEventListener("touchend", function(e) {
+        if (e.target == canvas) {
+          e.preventDefault();
+        }
+      }, false);
+      document.body.addEventListener("touchmove", function(e) {
+        if (e.target == canvas) {
+          e.preventDefault();
+        }
+      }, false);
+
+      (function drawLoop() {
+        requestAnimFrame(drawLoop);
+        renderCanvas();
+      })();
+
+      function clearCanvas() {
+        canvas.width = canvas.width;
+      }
+      // Prevent scrolling when touching the canvas N
+
+      // Set up the UI
+      var sigText = document.getElementById("ttd_dataUrl1");
+      var sigImage = document.getElementById("ttd_image1");
+      var clearBtn = document.getElementById("ttd_clearBtn1");
+      var submitBtn = document.getElementById("ttd_submitBtn1");
+      clearBtn.addEventListener("click", function(e) {
+        clearCanvas();
+        sigText.innerHTML = "Data URL for your signature will go here!";
+        sigImage.setAttribute("src", "");
+      }, false);
+      submitBtn.addEventListener("click", function(e) {
+        var dataUrl = canvas.toDataURL();
+        sigText.innerHTML = dataUrl;
+        sigImage.setAttribute("src", dataUrl);
+      }, false);
+
+    })();
+    // ttd S 1
+
+    // ttd 2
+    (function() {
+      window.requestAnimFrame = (function(callback) {
+        return window.requestAnimationFrame ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame ||
+          window.oRequestAnimationFrame ||
+          window.msRequestAnimaitonFrame ||
+          function(callback) {
+            window.setTimeout(callback, 1000 / 60);
+          };
+      })();
+
+      var canvas = document.getElementById("ttd_canvas2");
+      var ctx = canvas.getContext("2d");
+      ctx.strokeStyle = "#222222";
+      ctx.lineWidth = 4;
+
+      var drawing = false;
+      var mousePos = {
+        x: 0,
+        y: 0
+      };
+
+
+      // --
+      var lastPos = mousePos;
+
+      canvas.addEventListener("mousedown", function(e) {
+        drawing = true;
+        lastPos = getMousePos(canvas, e);
+      }, false);
+
+      canvas.addEventListener("mouseup", function(e) {
+        drawing = false;
+      }, false);
+
+      canvas.addEventListener("mousemove", function(e) {
+        mousePos = getMousePos(canvas, e);
+      }, false);
+      // --
+
+      // Add touch event support for mobile
+      canvas.addEventListener("touchstart", function(e) {
+
+      }, false);
+
+      canvas.addEventListener("touchmove", function(e) {
+        var touch = e.touches[0];
+        var me = new MouseEvent("mousemove", {
+          clientX: touch.clientX,
+          clientY: touch.clientY
+        });
+        canvas.dispatchEvent(me);
+      }, false);
+
+      canvas.addEventListener("touchstart", function(e) {
+        mousePos = getTouchPos(canvas, e);
+        var touch = e.touches[0];
+        var me = new MouseEvent("mousedown", {
+          clientX: touch.clientX,
+          clientY: touch.clientY
+        });
+        canvas.dispatchEvent(me);
+      }, false);
+
+      canvas.addEventListener("touchend", function(e) {
+        var me = new MouseEvent("mouseup", {});
+        canvas.dispatchEvent(me);
+      }, false);
+
+      function getMousePos(canvasDom, mouseEvent) {
+        var rect = canvasDom.getBoundingClientRect();
+        return {
+          x: mouseEvent.clientX - rect.left,
+          y: mouseEvent.clientY - rect.top
+        }
+      }
+
+      function getTouchPos(canvasDom, touchEvent) {
+        var rect = canvasDom.getBoundingClientRect();
+        return {
+          x: touchEvent.touches[0].clientX - rect.left,
+          y: touchEvent.touches[0].clientY - rect.top
+        }
+      }
+
+      function renderCanvas() {
+        if (drawing) {
+          ctx.moveTo(lastPos.x, lastPos.y);
+          ctx.lineTo(mousePos.x, mousePos.y);
+          ctx.stroke();
+          lastPos = mousePos;
+        }
+      }
+      // Add touch event support for mobile N
+
+      // Prevent scrolling when touching the canvas
+      document.body.addEventListener("touchstart", function(e) {
+        if (e.target == canvas) {
+          e.preventDefault();
+        }
+      }, false);
+      document.body.addEventListener("touchend", function(e) {
+        if (e.target == canvas) {
+          e.preventDefault();
+        }
+      }, false);
+      document.body.addEventListener("touchmove", function(e) {
+        if (e.target == canvas) {
+          e.preventDefault();
+        }
+      }, false);
+
+      (function drawLoop() {
+        requestAnimFrame(drawLoop);
+        renderCanvas();
+      })();
+
+      function clearCanvas() {
+        canvas.width = canvas.width;
+      }
+      // Prevent scrolling when touching the canvas N
+
+      // Set up the UI
+      var sigText = document.getElementById("ttd_dataUrl2");
+      var sigImage = document.getElementById("ttd_image2");
+      var clearBtn = document.getElementById("ttd_clearBtn2");
+      var submitBtn = document.getElementById("ttd_submitBtn2");
+      clearBtn.addEventListener("click", function(e) {
+        clearCanvas();
+        sigText.innerHTML = "Data URL for your signature will go here!";
+        sigImage.setAttribute("src", "");
+      }, false);
+      submitBtn.addEventListener("click", function(e) {
+        var dataUrl = canvas.toDataURL();
+        sigText.innerHTML = dataUrl;
+        sigImage.setAttribute("src", dataUrl);
+      }, false);
+
+    })();
+    // ttd S 2
+// FUNGSI TTD DIGITAL END
+</script>
 @endpush
--- resources/views/pages/admin/PPM/lk_inspeksi/index.blade.php
+++ resources/views/pages/admin/PPM/lk_inspeksi/index.blade.php
@@ -111,6 +111,71 @@
   </div> <!-- /.content -->
 </div> <!-- /.content-wrapper -->
 @push('addon-script')
-<script src="{{ asset('js/lkInspeksi_index.js') }}"></script>
+<script>
+  // FUNGSI AUTOFILL
+$(document).ready(function() {
+    $('select[id="lokasi_alat"]').on('change', function() {
+      var stateID = $(this).val();
+      if (stateID) {
+        $.ajax({
+          url: '/dashboard/ppm/lk_inspeksi/' + stateID,
+          type: "GET",
+          dataType: "json",
+          success: function(data) {
+            if (data.length > 0) {
+              data.forEach((item, index) => {
+                let i = index + 1; // Mulai dari 1
+                $('input[id="nama_alat_' + i + '"]').val(item.id_aset + '_' + item.nama_alat);
+                $('input[id="nomer_seri_' + i + '"]').val(item.serial_number);
+              });
+            } else {
+              $('input[id^="nama_alat_"]').val('');
+              $('input[id^="nomer_seri_"]').val('');
+            }
+          }
+        });
+      }
+    });
+  });
+  // FUNGSI AUTOFILL END
+</script>
+<script>
+  // FUNGSI BUAT ROW
+  $(document).ready(function() {
+    let rowCount = 1; // Menyimpan jumlah baris
+    const maxRows = 50; // Maksimal baris
+  
+    // Fungsi untuk menambah baris ke tabel
+    $("#addRow").click(function() {
+      if (rowCount > maxRows) {
+        alert("Maksimal row telah tercapai!");
+        return; // Menghentikan eksekusi jika sudah mencapai batas
+      }
+  
+      let newRow =
+        `<tr>
+            <td>${rowCount}</td>
+            <td><input name="nama_alat_${rowCount}" id="nama_alat_${rowCount}" type="text" class="form-control"></td>
+            <td><input name="nomer_seri_${rowCount}" id="nomer_seri_${rowCount}" type="text" class="form-control"></td>
+            <td><input name="periksa_fisik_${rowCount}" type="checkbox" class="form-check-input" value="Ya" checked></td>
+            <td><input name="lengkap_alat_${rowCount}" type="checkbox" class="form-check-input" value="Ya" checked></td>
+            <td><input name="fungsi_alat_${rowCount}" type="checkbox" class="form-check-input" value="Ya" checked></td>
+            <td><input name="catatan_${rowCount}" type="text" class="form-control"></td>
+            <td><button type="button" class="removeRow">Hapus</button></td>
+        </tr>`;
+  
+      // Menambah baris baru ke tbody
+      $("#dynamicTable tbody").append(newRow);
+      rowCount++; // Menambah nomor ID untuk input berikutnya
+    });
+  
+    // Fungsi untuk menghapus baris
+    $(document).on("click", ".removeRow", function() {
+      $(this).closest("tr").remove();
+      rowCount--; // Mengurangi rowCount ketika baris dihapus
+    });
+  });
+  // FUNGSI BUAT ROW END
+</script>
 @endpush
 @endsection
