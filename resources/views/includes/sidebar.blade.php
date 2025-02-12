17990cf81086f7f8cce913f537d2b0c82bfcd6b1
fahillan18899 <fahillan@gmail.com>
2025-02-12 08:37:28 +0700

    perbaikan side bar fixed

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,3 +1,21 @@
+<style>
+  .main-sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh; /* Agar sidebar penuh tinggi */
+    width: 250px; /* Atur lebar sidebar */
+    overflow-y: auto; /*Tambahkan scroll jika konten melebihi tinggi */
+    background-color: #042a4a; /* Warna background sesuai tema */
+    z-index: 1000; /* Pastikan sidebar di atas konten lain */
+    font-family: 'Alegreya Sans', sans-serif;
+    -webkit-transition: -webkit-transform 0.3s ease-in-out, width 0.3s ease-in-out;
+    -webkit-transition: width 0.3s ease-in-out, -webkit-transform 0.3s ease-in-out;
+    transition: width 0.3s ease-in-out, -webkit-transform 0.3s ease-in-out;
+    transition: transform 0.3s ease-in-out, width 0.3s ease-in-out;
+    transition: transform 0.3s ease-in-out, width 0.3s ease-in-out, -webkit-transform 0.3s ease-in-out;
+}
+</style>
 <aside class="main-sidebar">
   <!-- sidebar -->
   <div class="sidebar">
