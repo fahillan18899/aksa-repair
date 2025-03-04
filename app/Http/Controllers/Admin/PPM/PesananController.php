6bebfded0bea38c39ca09289e5c71071ac0f33e3
fahillan18899 <fahillan@gmail.com>
2025-03-04 08:53:00 +0700

    perbaikan halaman pesanan ke -2

--- app/Http/Controllers/Admin/PPM/PesananController.php
+++ app/Http/Controllers/Admin/PPM/PesananController.php
@@ -2,53 +2,26 @@
 
 namespace App\Http\Controllers\Admin\PPM;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 use App\Models\Pesanan;
 use App\Models\Registrasi;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class PesananController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
 
         $kodeRs_ = Auth::user()->kode_rs;
         $items = DB::table('pesanans')->where('kode_rs', $kodeRs_)->get();
-        $dataInv = Registrasi::where('kode_rs', Auth::user()->kode_rs)->get();
-        return view('pages.admin.PPM.pesanan.index', [
-
-            'items' => $items,
-            'dataInv' => $dataInv
-            
-        ]);
+        return view('pages.admin.PPM.pesanan.index',compact('items'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $dataPesanan =  $request->validate([
+        $data = $request->validate([
             'id' => 'unique:pesanans',
             'nama_req' => '',
             'merek_req' => '',
@@ -58,56 +31,13 @@ class PesananController extends Controller
             'pelapor_req' => '',
             'tanggal_req' => '',
             'kode_rs' => '',
-        ], [
-            'id.unique' => 'Aset Sudah Dilaporkan'
-        ]);
+        ], ['id.unique' => 'Aset Sudah Dilaporkan']);
 
-        $dataPesanan['kode_rs'] = Auth::user()->kode_rs;
-        Pesanan::create($dataPesanan);
-        return redirect('/dashboard/ppm/pesanan')
-            ->with('success', 'Data Alat Berhasil di Tambahkan.');
+        Pesanan::create(array_merge($data, ['kode_rs' => auth()->user()->kode_rs]));
+        session()->flash('success', 'Data Berhasil Terkirim');
+        return redirect('/dashboard/ppm/pesanan');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $item = Pesanan::where('id', $id)->where('kode_rs', Auth::user()->kode_rs)->first();
@@ -120,4 +50,24 @@ class PesananController extends Controller
       $pesanan = Registrasi::where("id_aset", $id)->get();
       return json_encode($pesanan);
     }
+
+    // public function create()
+    // {
+    //     //
+    // }
+
+    // public function update(Request $request, $id)
+    // {
+    //     //
+    // }
+
+    // public function show($id)
+    // {
+    //     //
+    // }
+
+    // public function edit($id)
+    // {
+    //     //
+    // }
 }
--- resources/views/pages/admin/PPM/pesanan/index.blade.php
+++ resources/views/pages/admin/PPM/pesanan/index.blade.php
@@ -4,11 +4,12 @@
 @section('title', 'Request Perbaikan')
 <style>
   .modal-body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%; /* Pastikan modal body penuh */
-}
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    /* Pastikan modal body penuh */
+  }
 </style>
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
@@ -26,12 +27,6 @@
   <div class="content">
     <!-- demo mode enable alert -->
     <div id="demoModeEnable"></div>
-    @if ($message = Session::get('success'))
-    <div class="alert alert-success">
-      <p>{{ $message }}</p>
-    </div>
-    @endif
-
     @if ($errors->any())
     <div class="alert alert-danger">
       <ul>
@@ -57,14 +52,14 @@
                   @csrf
                   <!-- Tambahkan tombol dan div untuk scanner -->
                   <div class="form-group row">
-                  <i class="text-danger">*</i><label for="note" class="col-xs-12 col-form-label">Klik Colom Pengisian id aset untuk load data setelah id aset muncul atau ada</label>
+                    <i class="text-danger">*</i><label for="note" class="col-xs-12 col-form-label">Klik Colom Pengisian id aset untuk load data setelah id aset muncul atau ada</label>
                   </div>
                   <div class="form-group row">
                     <label for="id" class="col-xs-3 col-form-label">Id Aset <i class="text-danger">*</i></label>
                     <div class="col-xs-9">
-                      <input name="id" type="text" class="form-control" id="id" 
-                      placeholder="id alat muncul setelah scan qr / ketik manual id aset alat" style="cursor: pointer;" 
-                      data-toggle="tooltip" data-placement="top" title="Klik disini untuk load data">
+                      <input name="id" type="text" class="form-control" id="id"
+                        placeholder="id alat muncul setelah scan qr / ketik manual id aset alat" style="cursor: pointer;"
+                        data-toggle="tooltip" data-placement="top" title="Klik disini untuk load data">
                     </div>
                   </div>
                   <div class="form-group row">
@@ -115,7 +110,7 @@
                   <div class="form-group row">
                     <div class="col-sm-offset-3 col-sm-6">
                       <div class="ui buttons">
-                        <button type="button" class="btn btn-primary mt-2" id="startScan" data-toggle="modal"data-target="#exampleModal">Scan QR</button>
+                        <button type="button" class="btn btn-primary mt-2" id="startScan" data-toggle="modal" data-target="#exampleModal">Scan QR</button>
                         <div class="or"></div>
                         <button class="ui positive button">Save</button>
                       </div>
@@ -133,56 +128,66 @@
     <div class="row">
       <div class="col-sm-12">
         <div class="panel panel-default thumbnail">
-
-          <div class="panel-body panel-form">
+          <div class="panel-heading no-print">
             <div class="row">
-              <div class="col-md-12 col-sm-12">
-
-                <!--TABEL-->
-                <table class="datatable table table-striped table-bordered" style="width:100%">
-                  <thead class="table-light">
-                    <tr>
-                      <th scope="col" class="none">No</th>
-                      <th scope="col">Id Aset</th>
-                      <th scope="col">Nama Alat</th>
-                      <th scope="col">Merek Alat</th>
-                      <th scope="col">Type Alat</th>
-                      <th scope="col">Serial Number</th>
-                      <th scope="col">Kerusakan Alat</th>
-                      <th scope="col">Pelapor</th>
-                      <th scope="col">Tanggal</th>
-                      <th scope="col">Tombol_Aksi_Table</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @forelse ($items as $index => $item)
-                    <tr>
-                      <td>{{ $index + 1 }}</td>
-                      <td>{{ $item->id }}</td>
-                      <td>{{ $item->nama_req }}</td>
-                      <td>{{ $item->merek_req }}</td>
-                      <td>{{ $item->type_req }}</td>
-                      <td>{{ $item->sn_req }}</td>
-                      <td>{{ $item->kerusakan_req }}</td>
-                      <td>{{ $item->pelapor_req }}</td>
-                      <td>{{ $item->tanggal_req }}</td>
-                      <td>
-                        <form action="{{ route('pesanan.destroy' ,$item->id) }}" method="POST" class="d-inline">
-                          @csrf
-                          @method('delete')
-                          <button class="btn btn-danger btn-xs" data-toggle="tooltip" data-placement="top" title="Validasi">
-                            Validasi Perbaikan
-                          </button>
-                        </form>
-                      </td>
-                    </tr>
-                    @empty
-                    @endforelse
-                  </tbody>
-                </table>
-                <!--TABEL-->
+              <div class="col-md-5">
               </div>
-              <div class="col-md-3"></div>
+              <div class="col-md-5">
+                <h2>Tabel Perbaikan</h2>
+              </div>
+            </div>
+          </div>
+          <div style="overflow-x:auto;">
+            <div class="panel-body panel-form">
+              <div class="row">
+                <div class="col-md-12 col-sm-12">
+
+                  <!--TABEL-->
+                  <table class="datatable table table-striped table-bordered" style="width:100%">
+                    <thead class="table-light">
+                      <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Id Aset</th>
+                        <th scope="col">Nama Alat</th>
+                        <th scope="col">Merek Alat</th>
+                        <th scope="col">Type Alat</th>
+                        <th scope="col">Serial Number</th>
+                        <th scope="col">Kerusakan Alat</th>
+                        <th scope="col">Pelapor</th>
+                        <th scope="col">Tanggal</th>
+                        <th scope="col">Tombol_Aksi_Table</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @forelse ($items as $index => $item)
+                      <tr>
+                        <td>{{ $index + 1 }}</td>
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->nama_req }}</td>
+                        <td>{{ $item->merek_req }}</td>
+                        <td>{{ $item->type_req }}</td>
+                        <td>{{ $item->sn_req }}</td>
+                        <td>{{ $item->kerusakan_req }}</td>
+                        <td>{{ $item->pelapor_req }}</td>
+                        <td>{{ $item->tanggal_req }}</td>
+                        <td>
+                          <form action="{{ route('pesanan.destroy' ,$item->id) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-danger btn-xs" data-toggle="tooltip" data-placement="top" title="Validasi">
+                              Validasi Perbaikan
+                            </button>
+                          </form>
+                        </td>
+                      </tr>
+                      @empty
+                      @endforelse
+                    </tbody>
+                  </table>
+                  <!--TABEL-->
+                </div>
+                <div class="col-md-3"></div>
+              </div>
             </div>
           </div>
         </div>
@@ -190,7 +195,7 @@
     </div>
   </div> <!-- /.content -->
 </div> <!-- /.content-wrapper -->
-<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button> 
+<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -212,6 +217,7 @@
 <!-- modal -->
 @endsection
 @push('addon-script')
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript">
   $('input[name="id"]').on('click', function() {
     var stateIDInv = $(this).val();
@@ -289,6 +295,19 @@
         });
       }
     });
+  });
+</script>
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    @if(session('success'))
+    Swal.fire({
+      icon: 'success',
+      title: 'Sukses!',
+      text: '{{ session("success") }}',
+      showConfirmButton: false,
+      timer: 2000
+    });
+    @endif
   });
 </script>
 @endpush
