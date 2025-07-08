8302bb8dca1661600b27d798224d78c559e4549f
fahillan18899 <fahillan@gmail.com>
2025-07-08 10:15:51 +0700

    menampilkan data repair di dashboard teknisi (Selesai)

--- app/Http/Controllers/Teknisi/DashboardTeknisiController.php
+++ app/Http/Controllers/Teknisi/DashboardTeknisiController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Teknisi;
 
 use App\Http\Controllers\Controller;
+use App\Models\DataBarang;
 use Illuminate\Http\Request;
 
 class DashboardTeknisiController extends Controller
 {
     public function dashboard_teknisi()
     {
-        return view('pages.teknisi.dashboard.index');
+        $itemSelesai = DataBarang::where('ket', '0')->get();
+        $itemPerbaikan = DataBarang::where('ket', '1')->get();
+        $countSelesai = DataBarang::where('ket', '0')->count();
+        $countPerbaikan = DataBarang::where('ket', '1')->count();
+        return view('pages.teknisi.dashboard.index',
+        compact('itemSelesai', 'itemPerbaikan', 'countSelesai', 'countPerbaikan'));
     }
 }
