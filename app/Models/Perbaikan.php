374d16aade444533a5110a28837d8e0403e45c3d
fahillan18899 <fahillan@gmail.com>
2026-06-04 06:03:28 +0700

    pembuatan db perbaikan

---
+++ app/Models/Perbaikan.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Perbaikan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'perbaikans';
+
+    protected $fillable = [
+        'id_alat',
+        'nama_alat',
+        'merek',
+        'type',
+        'seri',
+        'lokasi',
+        'kepala',
+        'teknisi',
+        'korektif',
+        'catatan',
+        'foto',
+    ];
+}
---
+++ database/migrations/2026_06_03_224752_create_perbaikans_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perbaikans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('id_alat')->nullable();
+            $table->text('nama_alat')->nullable();
+            $table->text('merek')->nullable();
+            $table->text('type')->nullable();
+            $table->text('seri')->nullable();
+            $table->text('lokasi')->nullable();
+            $table->text('kepala')->nullable();
+            $table->text('teknisi')->nullable();
+            $table->text('korektif')->nullable();
+            $table->text('catatan')->nullable();
+            $table->text('foto')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perbaikans');
+    }
+};
