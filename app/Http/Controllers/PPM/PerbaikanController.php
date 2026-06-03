89332515e26d1410d90fde54be6fc45ee4221b1c
fahillan18899 <fahillan@gmail.com>
2026-06-04 06:24:52 +0700

    pe,buatan store perbaikan

--- app/Http/Controllers/PPM/PerbaikanController.php
+++ app/Http/Controllers/PPM/PerbaikanController.php
@@ -5,13 +5,53 @@ namespace App\Http\Controllers\PPM;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Inv;
+use App\Models\Perbaikan;
 
 class PerbaikanController extends Controller
 {
     public function create(Request $request)
     {
         $qr = $request->qr;
+        $items = Perbaikan::latest()->get();
         $alat = Inv::where('id_alat', $qr)->firstOrFail();
-        return view('pages.admin.PPM.perbaikan.create', compact('qr', 'alat'));
+        return view('pages.admin.PPM.perbaikan.create', compact('qr', 'alat','items'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'id_alat'   => 'required',
+            'nama_alat' => 'required',
+            'merek'     => 'required',
+            'type'      => 'required',
+            'seri'      => 'required',
+            'lokasi'    => 'required',
+            'kepala'    => 'required',
+            'teknisi'   => 'required',
+            'korektif'  => 'required',
+            'catatan'   => 'required',
+            'foto'      => 'required',
+        ]);
+
+        $fotoPath = null;
+        if ($request->hasFile('foto')) {
+            $fotoPath = $request->file('foto')->store('alat', 'public');
+        }
+
+        Perbaikan::create([
+            'id_alat'   => $request->id_alat,
+            'nama_alat' => $request->nama_alat,
+            'merek'     => $request->merek,
+            'type'      => $request->type,
+            'seri'      => $request->seri,
+            'lokasi'    => $request->lokasi,
+            'kepala'    => $request->kepala,
+            'teknisi'   => $request->teknisi,
+            'korektif'  => $request->korektif,
+            'catatan'   => $request->catatan,
+            'foto'      => $fotoPath,
+        ]);
+
+        return back()->with('success', 'Data berhasil disimpan');
     }
 }
--- resources/views/pages/admin/PPM/perbaikan/create.blade.php
+++ resources/views/pages/admin/PPM/perbaikan/create.blade.php
@@ -132,7 +132,7 @@
             <div class="panel-body panel-form">
               <div class="row">
                 <div class="col-md-9 col-sm-12">
-                  <form action="" class="form-inner" enctype="multipart/form-data" method="post">
+                  <form action="{{route('perbaikan.store')}}" class="form-inner" enctype="multipart/form-data" method="post">
                       @csrf
 
                       <input type="hidden" name="id_alat" id="id_alat" value="{{ $qr }}">
@@ -227,7 +227,7 @@
 
           <div class="panel-heading no-print">
             <div class="">
-              <h1>Daftar Inventaris</h1>
+              <h1>Daftar Perbaikan</h1>
             </div>
           </div>
           <div class="table-responsive">
@@ -238,16 +238,55 @@
                   <table class="datatable table table-striped table-bordered" style="width:100%">
                     <thead class="table-light">
                       <th>Id</th>
+                      <th>Date</th>
                       <th>Nama</th>
                       <th>Merek</th>
                       <th>Type</th>
                       <th>Serial Number</th>
                       <th>Lokasi</th>
+                      <th>Kepala Ruang</th>
+                      <th>Teknisi</th>
+                      <th>Korektif</th>
+                      <th>Catatan</th>
                       <th>Foto</th>
                       {{-- <th>Tombol Aksi</th> --}}
                     </thead>
                     <tbody>
-                      
+                      @forelse($items as $item)
+                      <tr>
+                        <td>{{ $item->id_alat }}</td>
+                        <td>{{ $item->created_at }}</td>
+                        <td>{{ $item->nama_alat }}</td>
+                        <td>{{ $item->merek }}</td>
+                        <td>{{ $item->type }}</td>
+                        <td>{{ $item->seri }}</td>
+                        <td>{{ $item->lokasi }}</td>
+                        <td>{{ $item->kepala }}</td>
+                        <td>{{ $item->teknisi }}</td>
+                        <td>{{ $item->korektif }}</td>
+                        <td>{{ $item->catatan }}</td>
+                        <td>
+                          @if($item->foto)
+                          <img src="{{ asset('storage/' . $item->foto) }}"
+                              class="img-thumbnail"
+                              style="width:90px">
+                          @endif
+                        </td>
+                        {{-- <td>
+                          {{-- <a href="{{ route('inventaris.edit', $item->id) }}" class="btn btn-success btn-xs" data-toggle="tooltip" data-placement="top" title="Edit">
+                            <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                          </a> --}}
+                          {{-- <form action="" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger btn-xs" data-toggle="tooltip" data-placement="top" title="Hapus">
+                              <i class="fa fa-trash-o" aria-hidden="hidden"></i>
+                            </button>
+                          </form> 
+                        </td> --}}
+                      </tr>
+                      @empty
+                      @endforelse
                     </tbody>
                   </table>
                   <!--TABEL-->
